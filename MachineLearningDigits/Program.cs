eabe6a03f0b14eebfd736cf025f7ee144692dbac
korfu92 <konrad.korf@gmail.com>
2018-09-02 14:47:40 +0200

    1. reading data from excel file

--- MachineLearningDigits/Program.cs
+++ MachineLearningDigits/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MachineLearningDigits
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+            var trainingArray = File.ReadAllLines("ExcelFiles/trainingsample.csv").Skip(1);
+
+
+            foreach (var row in trainingArray.Take(5))
+            {
+                Console.WriteLine(row);
+                Console.WriteLine(" --- ");
+            }
+
+    
+            Console.ReadLine();
 
         }
     }
