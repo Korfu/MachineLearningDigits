65d7870b4a2c4dabd0799f7d9b35e1cf165fd2f9
korfu92 <konrad.korf@gmail.com>
2018-10-09 22:09:44 +0200

    some more F#

--- FSharp/Test.fsx
+++ FSharp/Test.fsx
@@ -1,14 +1,25 @@
 ﻿open System.IO
 type Observation = { Label:string; Pixels: int[] }
 let toObservation (csvData:string) =
-let columns = csvData.Split(',')
-let label = columns.[0]
-let pixels = columns.[1..] |> Array.map int
-{ Label = label; Pixels = pixels }
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label = label; Pixels = pixels }
+
 let reader path =
-let data = File.ReadAllLines path
-data.[1..]
-|> Array.map toObservation
-let trainingPath = @"C:\Users\konra\source\repos\MachineLearningDigits\MachineLearningDigits\ExcelFiles\
-trainingsample.csv"
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
+
+let trainingPath = @"C:\Users\konra\source\repos\MachineLearningDigits\MachineLearningDigits\ExcelFiles\trainingsample.csv"
+
 let trainingData = reader trainingPath
+
+let manhattanDistance (pixels1,pixels2) =
+    Array.zip pixels1 pixels2
+    |> Array.map (fun (x,y) -> abs (x-y))
+    |> Array.sum
+
+let array1 = [| "A";"B";"C" |]
+let array2 = [| 1 .. 3 |]
+let zipped = Array.zip array1 array2
